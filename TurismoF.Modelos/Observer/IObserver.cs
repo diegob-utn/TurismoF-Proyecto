44368dad9a45965eaaab33afcdfeeedadb2e460b
diegob-utn <nnbenavidesc@utn.edu.ec>
2025-06-28 01:28:55 -0500

    patrones de diseño

---
+++ TurismoF.Modelos/Observer/IObserver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Collections.Generic;
+
+namespace TurismoF.Modelos.Observer
+{
+    // Observer
+    public interface IObserver
+    {
+        void Update(string mensaje);
+    }
+
+    // Subject
+    public interface ISubject
+    {
+        void Attach(IObserver observer);
+        void Detach(IObserver observer);
+        void Notify(string mensaje);
+    }
+
+    // Reserva como sujeto que notifica cambios
+    public partial class Reserva:ISubject
+    {
+        private List<IObserver> _observers = new();
+
+        public void Attach(IObserver observer) => _observers.Add(observer);
+        public void Detach(IObserver observer) => _observers.Remove(observer);
+        public void Notify(string mensaje)
+        {
+            foreach(var obs in _observers)
+                obs.Update(mensaje);
+        }
+
+        public void CambiarEstado(EstadoReserva nuevoEstado)
+        {
+            // ... lógica de cambio ...
+            Notify($"Su reserva cambió a: {nuevoEstado}");
+        }
+    }
+
+    // Cliente como observador
+    public class Cliente:IObserver
+    {
+        public string Email { get; set; }
+        public void Update(string mensaje)
+        {
+            // Aquí podrías enviar una notificación real (email, push, etc.)
+            Console.WriteLine($"Notificando a {Email}: {mensaje}");
+        }
+    }
+}
---
+++ TurismoF.Modelos/Strategy/ICalculadoraPrecio.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TurismoF.Modelos;
+using System.Collections.Generic;
+
+using TurismoF.Modelos;
+
+namespace TurismoF.Modelos.Factory
+{
+    // Producto base
+    public abstract class Boleto
+    {
+        public int Id { get; set; }
+        public CategoriaPasajero Categoria { get; set; }
+        public TipoAsiento TipoAsiento { get; set; }
+        // ... otras propiedades ...
+    }
+
+    // Productos concretos
+    public class BoletoEstandar:Boleto { }
+    public class BoletoPreferencial:Boleto { }
+
+    // Creador abstracto
+    public abstract class BoletoFactory
+    {
+        public abstract Boleto CrearBoleto(CategoriaPasajero categoria, TipoAsiento tipoAsiento);
+    }
+
+    // Creadores concretos
+    public class BoletoEstandarFactory:BoletoFactory
+    {
+        public override Boleto CrearBoleto(CategoriaPasajero categoria, TipoAsiento tipoAsiento)
+        {
+            return new BoletoEstandar
+            {
+                Categoria = categoria,
+                TipoAsiento = tipoAsiento
+            };
+        }
+    }
+
+    public class BoletoPreferencialFactory:BoletoFactory
+    {
+        public override Boleto CrearBoleto(CategoriaPasajero categoria, TipoAsiento tipoAsiento)
+        {
+            return new BoletoPreferencial
+            {
+                Categoria = categoria,
+                TipoAsiento = tipoAsiento
+            };
+        }
+    }
+}
